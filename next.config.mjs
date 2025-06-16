cc802d682a1a4b8241131695bd64b6579abe77c2
Aarthi Honguthi <aarthihonguthi025@gmail.com>
2025-06-16 18:47:33 +0530

    Disable ESLint during production build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Skip ESLint checks in production
+  },
+};
 
 export default nextConfig;
